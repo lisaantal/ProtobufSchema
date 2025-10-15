56a9ad9966eb345aa1d90a6a8dec7d5071dbf5ae
Lisa <78325361+lisaantal@users.noreply.github.com>
2025-10-15 20:02:49 +0700

    Add main.go
    Co-authored-by: Hajigur <66867581+hajigur69@users.noreply.github.com>

---
+++ cmd/protobufschema/main.go
@@ -0,0 +1,23 @@
+// cmd/protobufschema/main.go
+package main
+
+import (
+    "flag"
+    "log"
+    "os"
+
+    "protobufschema/internal/protobufschema"
+)
+
+func main() {
+    verbose := flag.Bool("verbose", false, "Enable verbose logging")
+    input := flag.String("input", "", "Input file path")
+    output := flag.String("output", "", "Output file path")
+    flag.Parse()
+
+    app := protobufschema.NewApp(*verbose)
+    if err := app.Run(*input, *output); err != nil {
+        log.Printf("Error: %v", err)
+        os.Exit(1)
+    }
+}
